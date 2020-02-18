c38aef49209845a81949239d4742d842fba9f990
Kit Kogan <kkogan.dev@gmail.com>
2020-02-18 11:04:46 -0600

    Initial commit

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+    render() {
+        return(
+            <div className="About">
+                <h2>About</h2>
+                <p>Info about org here</p>
+            </div>
+
+        );
+    }
+}
+
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+class Contact extends Component {
+    render() {
+        return(
+            <div className="Contact">
+                <h2>Contact</h2>
+                <p>Phone: 555-555-5555</p>
+                <p>Email: actor@bestactors.org</p>
+            </div>
+
+        );
+    }
+}
+
+export default Contact;
